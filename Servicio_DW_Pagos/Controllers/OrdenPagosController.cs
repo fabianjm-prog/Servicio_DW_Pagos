804ff108f233be0192d019fe426bbae24c96d1a0
fabianjm-prog <fabianloky614@gmail.com>
2025-07-08 22:06:09 -0600

    Crud de Ordenes

--- Servicio_DW_Pagos/Controllers/OrdenPagosController.cs
+++ Servicio_DW_Pagos/Controllers/OrdenPagosController.cs
@@ -90,5 +90,70 @@ namespace Servicio_DW_Pagos.Controllers
 
             return StatusCode(StatusCodes.Status200OK, new { value = listaOrdenes });
         }
+
+        [HttpDelete]
+        [Route("Eliminar/{ID_Orden}")]
+        public async Task<IActionResult> EliminarOrden(int ID_Orden)
+        {
+            if (ID_Orden <= 0)
+            {
+                return BadRequest(new { mensaje = "El ID proporcionado no es valido." });
+            }
+
+            try
+            {
+                var movimiento = await _context.Orden_Pago.FirstOrDefaultAsync(m => m.ID_Orden == ID_Orden);
+
+                if (movimiento == null)
+                {
+                    return NotFound(new { mensaje = "La orden con ID" + ID_Orden + "no econtrada" });
+                }
+
+                _context.Orden_Pago.Remove(movimiento);
+                await _context.SaveChangesAsync();
+
+                return Ok(new { mensaje = "Orden de pago eliminada correctamente.", value = movimiento });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { mensaje = "Error al eliminar", error = ex.InnerException?.Message ?? ex.Message });
+            }
+        }
+
+
+
+        [HttpPut("Actualizar/{id}")]
+        public async Task<IActionResult> ActualizarOrden(int id, [FromBody] Orden_Pago OrdenPago)
+        {
+            if (id != OrdenPago.ID_Orden)
+            {
+                return BadRequest(new { mensaje = "El ID de Orden no existe" });
+            }
+
+            var OrdenEx = await _context.Orden_Pago.FindAsync(id);
+            if (OrdenEx == null)
+            {
+                return NotFound(new { mensaje = "Orden no encontrada" });
+            }
+
+            OrdenEx.ID_Orden = OrdenEx.ID_Orden;
+
+            //Cris aqui tenes que agregar los campos que se tienen que actualizar
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                return Ok(new { mensaje = "Orden de pago actualizado correctamente." });
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, new { mensaje = "Error al actualizar Orden de pago.", detalle = ex.Message });
+            }
+        }
+
+
+        // aqui recuerde agregar los metodos que necesite para el controlador de Ordenes de Pago 
+        // que estan en el proyecto el el documento. 
+        //adapta todo 
     }
 }
